f8c53555489d79ddd930179b879c9af37b297873
Lucy Follansbee <lfollansbee@gmail.com>
2016-08-29 11:51:27 -0600

    using new sidenav that is pinned

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-  $('.parallax').parallax();
-});
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+// });
 
 // Initialize collapse button
-$(".button-collapse").sideNav();
+// $(".button-collapse").sideNav();
+
+$(document).ready(function(){
+    $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+  });
