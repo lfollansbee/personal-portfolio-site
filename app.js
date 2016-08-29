8555f2c80421c7abe4ab603ac2ca3e6300d691f2
Lucy Follansbee <lfollansbee@gmail.com>
2016-08-29 13:58:47 -0600

    removing lines in skills section

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// $(document).ready(function(){
-//   $('.parallax').parallax();
-// });
-
-// Initialize collapse button
-// $(".button-collapse").sideNav();
-
 $(document).ready(function(){
     $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
   });
